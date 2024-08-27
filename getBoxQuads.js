64092fbb4c1f9882bd0c8536cc77b5c1aed338e7
jogibear9988 <jochen.kuehner@gmx.de>
2024-08-27 20:07:44 +0200

    null ref fix

--- getBoxQuads.js
+++ getBoxQuads.js
@@ -123,7 +123,7 @@ function transformPointBox(point, box, style, operator) {
 export function getBoxQuads(node, options) {
     let { width, height } = getElementSize(node);
     /** @type {DOMMatrix} */
-    let originalElementAndAllParentsMultipliedMatrix = getResultingTransformationBetweenElementAndAllAncestors(node, options?.relativeTo ?? (node.ownerDocument.defaultView ?? window).document.body.parentElement, options.iframes);
+    let originalElementAndAllParentsMultipliedMatrix = getResultingTransformationBetweenElementAndAllAncestors(node, options?.relativeTo ?? (node.ownerDocument.defaultView ?? window).document.body.parentElement, options?.iframes);
 
     let arr = [{ x: 0, y: 0 }, { x: width, y: 0 }, { x: width, y: height }, { x: 0, y: height }];
     /** @type { [DOMPoint, DOMPoint, DOMPoint, DOMPoint] } */
